391c47595dd328e549be0f07322e8f9b5a5ea8ce
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-05-02 21:28:19 -0400

    Have build errors @pnp etc...

--- src/webparts/fpsPageInfo/loc/mystrings.d.ts
+++ src/webparts/fpsPageInfo/loc/mystrings.d.ts
@@ -6,6 +6,16 @@ declare interface IFpsPageInfoWebPartStrings {
   AppLocalEnvironmentTeams: string;
   AppSharePointEnvironment: string;
   AppTeamsTabEnvironment: string;
+
+  //Added these for AdvancedPageProperties webpart as component
+  PropertyPaneDescription2: string;
+  SelectionGroupName: string;
+  TitleFieldLabel: string;
+  SelectedPropertiesFieldLabel: string;
+  PropPaneAddButtonText: string;
+  PropPaneDeleteButtonText: string;
+  LogAppName: string;
+
 }
 
 declare module 'FpsPageInfoWebPartStrings' {
