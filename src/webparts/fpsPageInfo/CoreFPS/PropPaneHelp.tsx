c5f4db8796e627e24c77efa9f9eb483f0dbdfb83
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-07-22 21:02:11 -0400

    close #91

--- src/webparts/fpsPageInfo/CoreFPS/PropPaneHelp.tsx
+++ src/webparts/fpsPageInfo/CoreFPS/PropPaneHelp.tsx
@@ -14,7 +14,7 @@ import { defaultBannerCommandStyles, } from "@mikezimm/npmfunctions/dist/HelpPan
 
 import ReactJson from "react-json-view";
 import { FontWeights } from 'office-ui-fabric-react';
-import { BannerHelp, FPSBasicHelp, FPSExpandHelp, ImportHelp, SinglePageAppHelp, VisitorHelp, SitePresetsInfo } from '@mikezimm/npmfunctions/dist/PropPaneHelp/FPSCommonOnNpm';
+import { BannerHelp, FPSBasicHelp, FPSExpandHelp, ImportHelp, SinglePageAppHelp, VisitorHelp, PinMeHelp, SitePresetsInfo } from '@mikezimm/npmfunctions/dist/PropPaneHelp/FPSCommonOnNpm';
 
 import {HandleBarReplacements } from '@mikezimm/npmfunctions/dist/Services/Strings/handleBars';
 
@@ -75,24 +75,7 @@ export function getWebPartHelpElement ( sitePresets : ISitePreConfigProps ) {
         //   selectedKey={ null }
         >
 
-        <PivotItem headerText={ 'Pin Me' } > 
-          <div className={ 'fps-pph-content' }>
-            <div className={ 'fps-pph-topic' }>Default Location</div>
-            <div>
-              <li><b>normal - </b>Web part loads on page where you put it</li>
-              <li><b>Pin Expanded - </b>Web part loads Pinned in upper right corner fully expanded</li>
-              <li><b>Pin Collapsed - </b>Web part loads Pinned in upper right corner collapsed</li>
-            </div>
-            <div className={ 'fps-pph-topic' }>Force Pin State</div>
-            <div>
-              <li><b>Let user change - </b>End user can move the web part from Pinned to Normal location at any time</li>
-              <li><b>Enforce no Toggle - </b>End user can not toggle the position of the web part.
-                <p>With Enforcing pin, the end user will always be able to expand or collapse the web part.</p>
-                <p>Be sure to test experience by loading the page with the browser shrunk to size of a phone.  Consider end user experience trying to navigate your page.</p>
-              </li>
-            </div>       
-          </div>
-        </PivotItem>
+        { PinMeHelp }
       
         <PivotItem headerText={ 'Table of Contents' } > 
           <div className={ 'fps-pph-content' }>
