4396bbc36cfab44f5138de3d655163992a8ed657
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-07-29 16:52:26 -0400

    Revert "Revert "web part does not load - crashes""
    This reverts commit 4192fb7ab68adb77e55ea3adff4efc3f980fdfd7.

--- src/webparts/fpsPageInfo/CoreFPS/BannerStyleFunctions.ts
+++ src/webparts/fpsPageInfo/CoreFPS/BannerStyleFunctions.ts
@@ -4,9 +4,10 @@ import { bannerThemes, bannerThemeKeys, makeCSSPropPaneString, createBannerStyle
 //Move this to @mikezimm/npmfunctions/dist/HelpPanelOnNPM/onNpm/defaults
 export const DefaultBannerThemes : any = {
     '/sites/lifenet': 'corpWhite1',
-    '/financemanual/manual': 'corpWhite1',
-    '/financemanual/test': 'corpWhite1',
-}
+    '/financemanual/manual': 'greenLight',
+    '/financemanual/test': 'redDark',
+    '/financemanual/help': 'Ukraine',
+};
 
 /**
  * 
